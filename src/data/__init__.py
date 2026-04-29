5662017c8cf1e4cb97838bf08bf4943208564f39
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-04-29 13:21:31 -0400

    feat: extended format support (165+ formats)
    - UniversalSlideReader: auto-selects OpenSlide/Bio-Formats
    - BioFormatsReader: python-bioformats wrapper
    - CZI, LIF, VSI, ND2, OIB, OIF, OIR, LSM, ZVI support
    - open_slide() convenience function
    - get_supported_formats() introspection

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,18 @@
 """Data loading and preprocessing modules."""
 
 from .loaders import MultimodalDataset, collate_multimodal
+from .format_support import (
+    UniversalSlideReader,
+    BioFormatsReader,
+    open_slide,
+    get_supported_formats,
+)
 
-__all__ = ["MultimodalDataset", "collate_multimodal"]
+__all__ = [
+    "MultimodalDataset",
+    "collate_multimodal",
+    "UniversalSlideReader",
+    "BioFormatsReader",
+    "open_slide",
+    "get_supported_formats",
+]
