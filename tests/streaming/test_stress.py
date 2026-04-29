aaa15d8b6304708dd2195c485c593870a95b7972
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-04-29 13:50:13 -0400

    fix: resolve CI import errors - AuditLogger, MemoryOptimizer, PathologyFLClient, add faiss/redis deps

--- tests/streaming/test_stress.py
+++ tests/streaming/test_stress.py
@@ -25,7 +25,7 @@ from src.streaming.attention_aggregator import StreamingAttentionAggregator
 
 # Import streaming components
 from src.streaming.gpu_pipeline import GPUPipeline
-from src.streaming.memory_optimizer import MemoryConfig, MemoryOptimizer
+from src.streaming.memory_optimizer import MemoryPoolManager, MemoryMonitor
 from src.streaming.model_optimizer import OptimizationConfig
 from src.streaming.parallel_pipeline import ParallelConfig, ParallelPipeline
 
