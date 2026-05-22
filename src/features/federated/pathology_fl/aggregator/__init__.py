0323e2a26792bcac36e87d8a74d65a9ecfdf715a
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-05-22 00:13:42 -0400

    Export explicit weighted aggregator

--- src/features/federated/pathology_fl/aggregator/__init__.py
+++ src/features/federated/pathology_fl/aggregator/__init__.py
@@ -3,9 +3,11 @@
 from src.features.federated.pathology_fl.aggregator.fedavg import FedAvgAggregator
 from src.features.federated.pathology_fl.aggregator.pathology_fl import PathologyFLAggregator
 from src.features.federated.pathology_fl.aggregator.secure import SecureAggregator
+from src.features.federated.pathology_fl.aggregator.weighted import ExplicitWeightedAggregator
 
 __all__ = [
     "FedAvgAggregator",
     "PathologyFLAggregator",
     "SecureAggregator",
+    "ExplicitWeightedAggregator",
 ]
