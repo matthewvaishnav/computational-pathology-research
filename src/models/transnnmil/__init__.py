85bbe0f365e17b7553eced7e043a9daadf5db7e1
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-07-18 13:05:50 -0400

    Export corrected TransnnMIL branch-token variant

--- src/models/transnnmil/__init__.py
+++ src/models/transnnmil/__init__.py
@@ -1,17 +1,21 @@
-"""TransnnMIL v2.0 - 3-branch MIL architecture."""
+"""TransnnMIL model family."""
 
 from src.models.transnnmil.adaptive_pruning import *
+from src.models.transnnmil.branch_token_fusion import *
 from src.models.transnnmil.graph_cache import *
 from src.models.transnnmil.hierarchical_pooling import *
 from src.models.transnnmil.topology_branch import *
 from src.models.transnnmil.transnnmil import *
+from src.models.transnnmil.transnnmil_branch_token import *
 from src.models.transnnmil.transnnmil_v2 import *
 
 __all__ = [
     "adaptive_pruning",
+    "branch_token_fusion",
     "graph_cache",
     "hierarchical_pooling",
     "topology_branch",
     "transnnmil",
+    "transnnmil_branch_token",
     "transnnmil_v2",
 ]
