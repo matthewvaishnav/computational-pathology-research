e51c8cce397339ce59775324ab263d00220cbd32
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-07-19 14:16:00 -0400

    Fix import ordering for salvage PR

--- src/models/transnnmil/__init__.py
+++ src/models/transnnmil/__init__.py
@@ -1,21 +1,21 @@
 """TransnnMIL model family."""
 
 from src.models.transnnmil.adaptive_pruning import *
-from src.models.transnnmil.graph_cache import *
-from src.models.transnnmil.hierarchical_pooling import *
-from src.models.transnnmil.topology_branch import *
-from src.models.transnnmil.transnnmil import *
-from src.models.transnnmil.transnnmil_v2 import *
 from src.models.transnnmil.branch_token_fusion import (
     BranchAttentionFusion,
     BranchConcatFusion,
     BranchGateFusion,
 )
+from src.models.transnnmil.graph_cache import *
+from src.models.transnnmil.hierarchical_pooling import *
+from src.models.transnnmil.topology_branch import *
+from src.models.transnnmil.transnnmil import *
 from src.models.transnnmil.transnnmil_branch_token import (
     TransnnMILBranchAttentionExperimental,
     TransnnMILConcatExperimental,
     TransnnMILGateExperimental,
 )
+from src.models.transnnmil.transnnmil_v2 import *
 
 __all__ = [
     "adaptive_pruning",
