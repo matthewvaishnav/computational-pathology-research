95df5fc96ff518c6234127b36b7342227a010b8c
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-07-19 14:12:50 -0400

    Export experimental TransnnMIL fusion variants

--- src/models/transnnmil/__init__.py
+++ src/models/transnnmil/__init__.py
@@ -1,4 +1,4 @@
-"""TransnnMIL v2.0 - 3-branch MIL architecture."""
+"""TransnnMIL model family."""
 
 from src.models.transnnmil.adaptive_pruning import *
 from src.models.transnnmil.graph_cache import *
@@ -6,6 +6,16 @@ from src.models.transnnmil.hierarchical_pooling import *
 from src.models.transnnmil.topology_branch import *
 from src.models.transnnmil.transnnmil import *
 from src.models.transnnmil.transnnmil_v2 import *
+from src.models.transnnmil.branch_token_fusion import (
+    BranchAttentionFusion,
+    BranchConcatFusion,
+    BranchGateFusion,
+)
+from src.models.transnnmil.transnnmil_branch_token import (
+    TransnnMILBranchAttentionExperimental,
+    TransnnMILConcatExperimental,
+    TransnnMILGateExperimental,
+)
 
 __all__ = [
     "adaptive_pruning",
@@ -14,4 +24,10 @@ __all__ = [
     "topology_branch",
     "transnnmil",
     "transnnmil_v2",
+    "BranchAttentionFusion",
+    "BranchConcatFusion",
+    "BranchGateFusion",
+    "TransnnMILBranchAttentionExperimental",
+    "TransnnMILConcatExperimental",
+    "TransnnMILGateExperimental",
 ]
